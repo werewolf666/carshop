af781625c1684e6c522074ebe4a285c2c5c023ca
werewolf666 <1031484669@qq.com>
2019-03-27 22:33:51 +0800

    first time

---
+++ Application/Admin/Controller/AdminController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class AdminController extends Controller {
+    public function lst(){
+        $this->display();
+    }
+
+    public function add(){
+        $this->display();
+    }
+
+    public function edit(){
+        $this->display();
+    }
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+//定义网站根目录
+define('SITEURL','http://1008.com/carshop/');
+
+//定义后台资源目录
+define('ADMIN_PUC',SITEURL.'Application/Admin/Public');
+
+//定义前台资源目录
+define('HOME_PUC',SITEURL.'Application/Home/Public');
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
